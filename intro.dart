42a7f5181642982e8491951cf8bce38d8c9e606b
lamiz-khan <khanlamiz8@gmail.com>
2025-04-20 12:45:08 +0500

    A program of Introduction According to Data type

---
+++ intro.dart
@@ -0,0 +1,17 @@
+void main (){
+    print ("INTRODUCTION:");
+    String name = "Lamiz khan";
+    print ("My name is $name.");
+
+     num age = 21;
+   print ("my age is $age.");
+   
+   String my_class = "Undergraduate";
+   print ("my class is $my_class.");
+   
+   double percentage = 77.33;
+   print ("my percentage is $percentage%");
+   
+   String prev_result = "A one grade";
+   print ("my previous result is $prev_result.");
+ }
